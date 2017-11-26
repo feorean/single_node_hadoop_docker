294741aea3226158b917e104a26283075917a5c8
Khalid Mammadov <xmamedov@hotmail.com>
2017-11-26 16:28:10 +0000

    Initial

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+#cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+#chmod 0600 ~/.ssh/authorized_keys
+
+
+service ssh start 
+
+cd /usr/local/hadoop/sbin/
+./start-dfs.sh
+./start-yarn.sh
+
+cd ~
+
+/bin/bash
+
